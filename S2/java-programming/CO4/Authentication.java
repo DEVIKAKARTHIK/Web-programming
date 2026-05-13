bf3ec15e4e00c2c830da8e78b0f45843025948cd
DEVIKAKARTHIK <devikakarthik2004@gmail.com>
2026-05-13 06:46:11 +0530

    Add user authentication with custom exceptions
    Implemented user authentication with exception handling for invalid username and password.

--- S2/java-programming/CO4/Authentication.java
+++ S2/java-programming/CO4/Authentication.java
@@ -1,11 +1,66 @@
-package CO4;
-import java.util.Scanner;
-public class Authentication {
-    
-        static final String USERNAME="username";
-        static final String PASSWORD="password";
-    public static void main(String args[]){
+// 3. Write a user defined exception class to authenticate the user name and password.
 
+import java.util.Scanner;
+
+public class Authentication {
+
+    // Actual username and password of the user
+    static final String USERNAME = "username";
+    static final String PASSWORD = "password";
+    
+    public static void main(String[] args) {
         
+        try {
+
+            // Read Username
+            Scanner sc = new Scanner(System.in);
+            System.out.print("Enter username: ");
+            String username = sc.next();
+            sc.nextLine();
+            
+            // Validate Username
+            if (!username.equals(USERNAME)) {
+                sc.close();
+                throw new InvalidUsernameException();
+            } 
+            
+            // Read Password
+            System.out.print("Enter password: ");
+            String password = sc.next();
+            sc.nextLine();
+            sc.close();
+
+            // Validate Password
+            if (!password.equals(PASSWORD)) throw new InvalidPasswordException();
+            
+            System.out.println("Logged In!");
+
+        } catch (InvalidUsernameException | InvalidPasswordException e) {
+            // Mulitcatch: Catching multiple exceptions in a single catch block
+            e.printStackTrace();
+        } catch (Exception e) {
+            return;
+        }
     }
+
+}
+
+class InvalidUsernameException extends RuntimeException {
+
+    InvalidUsernameException() {
+
+        super("Invalid Username!"); 
+
+    }
+
+}
+
+class InvalidPasswordException extends  RuntimeException {
+
+    InvalidPasswordException() {
+
+        super("Invalid Password!");
+
+    }
+
 }
